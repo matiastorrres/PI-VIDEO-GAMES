79e4a67d9f8e3b28df1472fa88320d4e6b217dca
matias <mmaattuumza@gmail.com>
2022-09-21 18:55:08 -0300

    heroku

--- api/src/controller/genre.js
+++ api/src/controller/genre.js
@@ -1,22 +1,22 @@
 const axios = require("axios");
-const {Genre} =require("../db")
-const {YOUR_API_KEY} = process.env;
-const getGenreApi = () =>{
-    return axios(`https://api.rawg.io/api/genres?key=${YOUR_API_KEY}`)
-    .then(resp=>resp.data.results)
-    .then(resp=>{
-        return resp.map(e=>{
-            return {name:e.name}
-        }) 
+const { Genre } = require("../db");
+const { YOUR_API_KEY } = process.env;
+const getGenreApi = () => {
+  return axios(`https://api.rawg.io/api/genres?key=${YOUR_API_KEY}`)
+    .then((resp) => resp.data.results)
+    .then((resp) => {
+      return resp.map((e) => {
+        return { name: e.name };
+      });
     })
-    .catch(()=>console.log("aca es el error"))   
-}
+    .catch((error) => console.log(error));
+};
 
-const getRengeDb = async () =>{
-    const renge = await Genre.findAll({
-       attributes:["name"]
-    })
-    return renge.map(e=>e.name)
-}
+const getRengeDb = async () => {
+  const renge = await Genre.findAll({
+    attributes: ["name"],
+  });
+  return renge.map((e) => e.name);
+};
 
-module.exports = { getGenreApi, getRengeDb } 
+module.exports = { getGenreApi, getRengeDb };
